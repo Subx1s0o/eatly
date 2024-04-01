800c37747234677d27b18da77de8fd39b79bb023
Subx1s0o <nazarsabardin@gmail.com>
2024-04-01 14:59:39 +0300

    remake script

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// const BurgerButtonOpen = document.getElementById("burger-button-open");
-
-// const BurgerButtonClose = document.getElementById("burger-button-close");
-
-// const BurgerOverlay = document.getElementById("burger-menu");
-
-// const BodyFlow = document.querySelector("body");
-
-// BurgerButtonOpen.addEventListener("click", () => {
-//   BurgerOverlay.classList.toggle("is-open");
-//   BodyFlow.classList.toggle("unscroll");
-// });
-
-// BurgerButtonClose.addEventListener("click", () => {
-//   BurgerOverlay.classList.toggle("is-open");
-//   BodyFlow.classList.toggle("unscroll");
-// });
 (() => {
   const mobileMenu = document.querySelector(".burger-overlay ");
   const openMenuBtn = document.querySelector(".burger_menu");
@@ -27,17 +10,16 @@
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
 
-    const scrollLockMethod = isMenuOpen
+    const scrollLockMethod = !isMenuOpen
       ? "disableBodyScroll"
-      : "enableBodyScroll"; // Переключение метода в зависимости от состояния меню
+      : "enableBodyScroll";
     bodyScrollLock[scrollLockMethod](document.body);
   };
-
   closeMenuLink.forEach((item) => item.addEventListener("click", toggleMenu));
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
 
-  // Закрыть мобильное меню на более широких экранах при изменении ориентации устройства
+  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
     mobileMenu.classList.remove("is-open");
