e8c5ddd28860e6712cb8179dcf33fb6800ed0fc3
Subx1s0o <nazarsabardin@gmail.com>
2024-04-02 18:37:12 +0300

    add test animations

--- js/script.js
+++ js/script.js
@@ -48,7 +48,63 @@ function trackScroll() {
 
 function goTop() {
   if (window.scrollY > 0) {
-    window.scrollBy(0, -25); //другий аргумент - це швидкість скролу
+    window.scrollBy(0, -20); //другий аргумент - це швидкість скролу
     setTimeout(goTop, 0);
   }
+}
+
+gsap.registerPlugin(ScrollTrigger);
+
+const tl1 = gsap.timeline({ defaults: { duration: 0.2 } });
+const tl2 = gsap.timeline({ defaults: { duration: 0.3 } });
+
+const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+if (mediaQuery.matches) {
+  tl1
+    .from(".logotype", { opacity: 0, x: -100 })
+    .from(".nav-li", { opacity: 0, y: -40, stagger: 0.05 })
+    .from(".header__buttons", { opacity: 0, x: 50 })
+    .from(".hero-text-first", { opacity: 0, y: 30 })
+    .from(".hero-title", { opacity: 0, x: -30 })
+    .from(".hero__image", { opacity: 0, x: 40 })
+    .from(".hero-text-second", { opacity: 0, x: -30 })
+    .from(".hero__buttons", { opacity: 0, y: 40 })
+    .from(".rating__content", { opacity: 0, y: 40 });
+
+  tl2.from([".features-h", ".features-p"], {
+    duration: 0.5,
+    opacity: 0,
+    y: 10,
+    scrollTrigger: {
+      trigger: ".features",
+      markers: true,
+      start: "-50% center ",
+      end: "100% center ",
+      scrub: false,
+    },
+  });
+} else {
+  tl1
+    .from(".logotype", { opacity: 0, x: -100 })
+    .from(".burger_menu", { opacity: 0, x: 30 })
+    .from(".hero-text-first", { opacity: 0, y: -40 })
+    .from(".hero-title", { opacity: 0, y: 30 })
+    .from(".hero-text-second", { opacity: 0, y: 30 })
+    .from(".hero__buttons", { opacity: 0, y: 40 })
+    .from(".rating__content", { opacity: 0, y: 40 })
+    .from(".hero__image", { opacity: 0, y: 40 });
+
+  tl2.from([".features-h", ".features-p"], {
+    duration: 0.5,
+    opacity: 0,
+    y: 10,
+    scrollTrigger: {
+      trigger: ".features",
+      markers: true,
+      start: "-15% center ",
+      end: "100% center ",
+      scrub: false,
+    },
+  });
 }
